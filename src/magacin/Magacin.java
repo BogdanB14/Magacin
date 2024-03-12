81beb40dab23194e2ee782da071694879540e9c1
BogdanB14 <bogdanblagojevic001@gmail.com>
2024-03-12 18:20:27 +0100

    Dodate magacin interfejs i metode

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -2,9 +2,29 @@ package magacin;
 
 import java.util.*;
 
+import specifikacijamagacin.InterfejsMagacin;
 
 
-public class Magacin {
+
+public class Magacin implements InterfejsMagacin{
 	private List<Artikal> artikli = new ArrayList<>();
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
